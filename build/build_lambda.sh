7e7525735b6592a279790cf9381b9a03e0d54c83
Sebastien Marc <sebmarc@yahoo.com>
2024-09-03 21:20:15 +0200

    adding build

--- build/build_lambda.sh
+++ build/build_lambda.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 echo "Cleaning up old build files"
-parent_dir=$(dirname "$(readlink -f "$0")")
+# Get the absolute path of the script
+script_path="$(realpath "$0")" || { echo "Failed to determine script path" >&2; exit 1; }
+# Extract the directory path
+script_dir="$(dirname "$script_path")"
+# Get the parent directory
+parent_dir="$(dirname "$script_dir")"
+echo "Cleaning up: $parent_dir/package"
 rm -rf $parent_dir/transcriptions.zip
 rm -rf $parent_dir/package
 mkdir $parent_dir/package
 echo "Installing dependencies"
 pip install --target $parent_dir/package boto3
 pip install --target $parent_dir/package assemblyai
+pip install --target $parent_dir/package mypy_boto3_s3
 echo "Creating package..."
 current_dir=$(pwd)
-cd $parent_dir/package
+cd "$parent_dir/package" || { echo "Failed to change to package directory" >&2; exit 1; }
 zip -r $parent_dir/transcriptions.zip .
-cd $parent_dir
+cd $parent_dir || { echo "Failed to change to parent directory" >&2; exit 1; }
 zip $parent_dir/transcriptions.zip transcribe_lambda.py
 zip -d $parent_dir/transcriptions.zip "*/__pycache__/*"
-cd $current_dir
 echo "Package done!"
